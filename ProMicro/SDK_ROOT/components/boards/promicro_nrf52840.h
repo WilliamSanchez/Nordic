77e24e50706187dc881143ab5bf84117a9031d18
WilliamSanchez <wsanchef@gmail.com>
2026-07-19 08:35:40 -0300

    [ProMicro] Simple blinky usin Makefile
    It is a base for new project using a simple Makefile and the
    adafruit_nrfutil tool.

---
+++ ProMicro/SDK_ROOT/components/boards/promicro_nrf52840.h
@@ -0,0 +1,7 @@
+#ifndef _PROMICRO_NRF52840_H
+#define _PROMICRO_NRF52840_H
+
+#define LED_PIN 15
+#define LED_PIN8 8
+
+#endif
---
+++ ProMicro/ble/_build/nrf52840_xxaa/nrf_log_default_backends.c.d
@@ -0,0 +1,137 @@
+_build/nrf52840_xxaa/nrf_log_default_backends.c.o: \
+ ../SDK_ROOT/components/libraries/log/src/nrf_log_default_backends.c \
+ ../SDK_ROOT/components/libraries/util/sdk_common.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdbool.h \
+ /usr/include/newlib/string.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/reent.h \
+ /usr/include/newlib/_ansi.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stddef.h \
+ /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/string.h \
+ config/sdk_config.h \
+ ../SDK_ROOT/components/libraries/util/nordic_common.h \
+ ../SDK_ROOT/modules/nrfx/mdk/compiler_abstraction.h \
+ ../SDK_ROOT/components/libraries/util/sdk_os.h \
+ ../SDK_ROOT/components/libraries/util/sdk_errors.h \
+ ../SDK_ROOT/components/softdevice/s140/headers/nrf_error.h \
+ ../SDK_ROOT/components/libraries/util/app_util.h \
+ ../SDK_ROOT/modules/nrfx/mdk/nrf.h \
+ ../SDK_ROOT/modules/nrfx/mdk/nrf52840.h \
+ ../SDK_ROOT/components/toolchain/cmsis/include/core_cm4.h \
+ ../SDK_ROOT/components/toolchain/cmsis/include/cmsis_version.h \
+ ../SDK_ROOT/components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../SDK_ROOT/components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../SDK_ROOT/components/toolchain/cmsis/include/mpu_armv7.h \
+ ../SDK_ROOT/modules/nrfx/mdk/system_nrf52840.h \
+ ../SDK_ROOT/modules/nrfx/mdk/nrf52840_bitfields.h \
+ ../SDK_ROOT/modules/nrfx/mdk/nrf51_to_nrf52840.h \
+ ../SDK_ROOT/modules/nrfx/mdk/nrf52_to_nrf52840.h \
+ ../SDK_ROOT/modules/nrfx/mdk/compiler_abstraction.h \
+ ../SDK_ROOT/components/softdevice/s140/headers/nrf52/nrf_mbr.h \
+ ../SDK_ROOT/components/softdevice/s140/headers/nrf_svc.h \
+ ../SDK_ROOT/components/libraries/util/sdk_macros.h \
+ ../SDK_ROOT/components/libraries/util/nrf_assert.h \
+ ../SDK_ROOT/components/libraries/log/nrf_log_default_backends.h \
+ ../SDK_ROOT/components/libraries/util/sdk_errors.h \
+ ../SDK_ROOT/components/libraries/log/nrf_log_ctrl.h \
+ ../SDK_ROOT/components/libraries/log/nrf_log_types.h \
+ ../SDK_ROOT/components/libraries/log/src/nrf_log_ctrl_internal.h \
+ ../SDK_ROOT/components/libraries/log/nrf_log_backend_interface.h \
+ ../SDK_ROOT/components/libraries/memobj/nrf_memobj.h \
+ /usr/include/newlib/stdlib.h /usr/include/newlib/machine/stdlib.h \
+ ../SDK_ROOT/components/libraries/balloc/nrf_balloc.h \
+ ../SDK_ROOT/components/libraries/util/app_util_platform.h \
+ ../SDK_ROOT/components/softdevice/s140/headers/nrf_soc.h \
+ ../SDK_ROOT/components/softdevice/s140/headers/nrf_svc.h \
+ ../SDK_ROOT/components/softdevice/s140/headers/nrf_error.h \
+ ../SDK_ROOT/components/softdevice/s140/headers/nrf_error_soc.h \
+ ../SDK_ROOT/components/softdevice/s140/headers/nrf_nvic.h \
+ ../SDK_ROOT/components/libraries/util/app_error.h \
+ /usr/include/newlib/stdio.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdarg.h \
+ /usr/include/newlib/sys/stdio.h \
+ ../SDK_ROOT/components/libraries/util/app_error_weak.h \
+ ../SDK_ROOT/components/libraries/util/app_util.h \
+ ../SDK_ROOT/components/libraries/log/nrf_log_instance.h \
+ ../SDK_ROOT/components/libraries/experimental_section_vars/nrf_section.h \
+ ../SDK_ROOT/components/libraries/util/nordic_common.h \
+ ../SDK_ROOT/components/libraries/log/src/nrf_log_internal.h \
+ ../SDK_ROOT/components/libraries/log/nrf_log_types.h \
+ ../SDK_ROOT/components/libraries/util/nrf_assert.h \
+ ../SDK_ROOT/components/libraries/log/nrf_log_backend_rtt.h
+../SDK_ROOT/components/libraries/util/sdk_common.h:
+/usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h:
+/usr/lib/gcc/arm-none-eabi/13.2.1/include/stdbool.h:
+/usr/include/newlib/string.h:
+/usr/include/newlib/_ansi.h:
+/usr/include/newlib/newlib.h:
+/usr/include/newlib/_newlib_version.h:
+/usr/include/newlib/sys/config.h:
+/usr/include/newlib/machine/ieeefp.h:
+/usr/include/newlib/sys/features.h:
+/usr/include/newlib/sys/reent.h:
+/usr/include/newlib/_ansi.h:
+/usr/lib/gcc/arm-none-eabi/13.2.1/include/stddef.h:
+/usr/include/newlib/sys/cdefs.h:
+/usr/include/newlib/machine/_default_types.h:
+/usr/include/newlib/sys/_types.h:
+/usr/include/newlib/machine/_types.h:
+/usr/include/newlib/sys/lock.h:
+/usr/include/newlib/sys/string.h:
+config/sdk_config.h:
+../SDK_ROOT/components/libraries/util/nordic_common.h:
+../SDK_ROOT/modules/nrfx/mdk/compiler_abstraction.h:
+../SDK_ROOT/components/libraries/util/sdk_os.h:
+../SDK_ROOT/components/libraries/util/sdk_errors.h:
+../SDK_ROOT/components/softdevice/s140/headers/nrf_error.h:
+../SDK_ROOT/components/libraries/util/app_util.h:
+../SDK_ROOT/modules/nrfx/mdk/nrf.h:
+../SDK_ROOT/modules/nrfx/mdk/nrf52840.h:
+../SDK_ROOT/components/toolchain/cmsis/include/core_cm4.h:
+../SDK_ROOT/components/toolchain/cmsis/include/cmsis_version.h:
+../SDK_ROOT/components/toolchain/cmsis/include/cmsis_compiler.h:
+../SDK_ROOT/components/toolchain/cmsis/include/cmsis_gcc.h:
+../SDK_ROOT/components/toolchain/cmsis/include/mpu_armv7.h:
+../SDK_ROOT/modules/nrfx/mdk/system_nrf52840.h:
+../SDK_ROOT/modules/nrfx/mdk/nrf52840_bitfields.h:
+../SDK_ROOT/modules/nrfx/mdk/nrf51_to_nrf52840.h:
+../SDK_ROOT/modules/nrfx/mdk/nrf52_to_nrf52840.h:
+../SDK_ROOT/modules/nrfx/mdk/compiler_abstraction.h:
+../SDK_ROOT/components/softdevice/s140/headers/nrf52/nrf_mbr.h:
+../SDK_ROOT/components/softdevice/s140/headers/nrf_svc.h:
+../SDK_ROOT/components/libraries/util/sdk_macros.h:
+../SDK_ROOT/components/libraries/util/nrf_assert.h:
+../SDK_ROOT/components/libraries/log/nrf_log_default_backends.h:
+../SDK_ROOT/components/libraries/util/sdk_errors.h:
+../SDK_ROOT/components/libraries/log/nrf_log_ctrl.h:
+../SDK_ROOT/components/libraries/log/nrf_log_types.h:
+../SDK_ROOT/components/libraries/log/src/nrf_log_ctrl_internal.h:
+../SDK_ROOT/components/libraries/log/nrf_log_backend_interface.h:
+../SDK_ROOT/components/libraries/memobj/nrf_memobj.h:
+/usr/include/newlib/stdlib.h:
+/usr/include/newlib/machine/stdlib.h:
+../SDK_ROOT/components/libraries/balloc/nrf_balloc.h:
+../SDK_ROOT/components/libraries/util/app_util_platform.h:
+../SDK_ROOT/components/softdevice/s140/headers/nrf_soc.h:
+../SDK_ROOT/components/softdevice/s140/headers/nrf_svc.h:
+../SDK_ROOT/components/softdevice/s140/headers/nrf_error.h:
+../SDK_ROOT/components/softdevice/s140/headers/nrf_error_soc.h:
+../SDK_ROOT/components/softdevice/s140/headers/nrf_nvic.h:
+../SDK_ROOT/components/libraries/util/app_error.h:
+/usr/include/newlib/stdio.h:
+/usr/lib/gcc/arm-none-eabi/13.2.1/include/stdarg.h:
+/usr/include/newlib/sys/stdio.h:
+../SDK_ROOT/components/libraries/util/app_error_weak.h:
+../SDK_ROOT/components/libraries/util/app_util.h:
+../SDK_ROOT/components/libraries/log/nrf_log_instance.h:
+../SDK_ROOT/components/libraries/experimental_section_vars/nrf_section.h:
+../SDK_ROOT/components/libraries/util/nordic_common.h:
+../SDK_ROOT/components/libraries/log/src/nrf_log_internal.h:
+../SDK_ROOT/components/libraries/log/nrf_log_types.h:
+../SDK_ROOT/components/libraries/util/nrf_assert.h:
+../SDK_ROOT/components/libraries/log/nrf_log_backend_rtt.h:
